d147ce2c81777d9c39ec42b894b929e136fec8dc
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-28 21:14:43 -0300

    feat: create user

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,9 +1,23 @@
-import { Query, Resolver } from "type-graphql";
+import { Mutation, Query, Resolver, Arg } from "type-graphql";
+import { User } from "../model/user";
+import crypto from "crypto";
 
 @Resolver()
 export class UserResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello World!";
+  private data: User[] = []
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("name") name: string
+  ) {
+    const user = {  id: crypto.randomUUID(), name }
+    
+    this.data.push(user);
+
+    return user;
   }
 }
